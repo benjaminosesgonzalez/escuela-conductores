0f6d260eed41a62cbe918df2a3d229e1040c824a
Benjamin Oses Gonzalez <benjamin.oses1901@alumnos.ubiobio.cl>
2026-04-25 05:20:25 -0400

    Habilitar "Polling" en Vite
    Modificación al archivo vite.config.js para que utlilice "polling" con el fin de detectar correctamente cambios en el cliente.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // <--- ESTA ES LA LÍNEA MÁGICA
+    },
+    host: true, // Esto asegura que sea accesible desde fuera del contenedor
+    strictPort: true,
+    port: 5173,
+  },
 })
