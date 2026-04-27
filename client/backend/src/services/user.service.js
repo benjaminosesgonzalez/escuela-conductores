0fd7de6b7a10a6c33161a2bed995db54f5b0bf95
jeanjeldres <jean.jeldres2101@alumnos.ubiobio.cl>
2026-04-26 21:49:12 -0400

    admin, creacion de secretarias y logica de matricula

--- client/backend/src/services/user.service.js
+++ client/backend/src/services/user.service.js
@@ -1,5 +1,8 @@
 import { AppDataSource } from "../config/configDB.js";
 import { User } from "../entities/user.entity.js";
+import { Alumno } from "../entities/alumno.entity.js";
+import { Profesor } from "../entities/profesor.entity.js";
+import { Funcionario } from "../entities/funcionario.entity.js";
 import bcrypt from "bcrypt";
 
 const userRepository = AppDataSource.getRepository(User);
@@ -10,9 +13,41 @@ export async function createUser(data) {
   const newUser = userRepository.create({
     email: data.email,
     password: hashedPassword,
+    rol: data.rol,
   });
 
-  return await userRepository.save(newUser);
+  const savedUser = await userRepository.save(newUser);
+
+  if (savedUser.rol === "alumno") {
+    const repo = AppDataSource.getRepository(Alumno);
+    await repo.save(
+      repo.create({
+        nombre: data.nombre,
+        rut: data.rut,
+        telefono: data.telefono,
+        id_usuario: savedUser.id,
+      }),
+    );
+  } else if (savedUser.rol === "profesor") {
+    const repo = AppDataSource.getRepository(Profesor);
+    await repo.save(
+      repo.create({
+        nombre: data.nombre,
+        telefono: data.telefono,
+        id_usuario: savedUser.id,
+      }),
+    );
+  } else if (savedUser.rol === "administracion") {
+    const repo = AppDataSource.getRepository(Funcionario);
+    await repo.save(
+      repo.create({
+        nombre: data.nombre,
+        id_usuario: savedUser.id,
+      }),
+    );
+  }
+
+  return savedUser;
 }
 
 export async function findUserByEmail(email) {
