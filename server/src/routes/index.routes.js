a78f6f8909d48edcc25cda1cc47b9cf307b954d6
Mauricio Giussepe Burgos Riquelme <mauricio.burgos2101@alumnos.ubiobio.cl>
2026-06-03 19:01:07 -0400

    mejoras y ajustes

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -3,7 +3,7 @@ import authRoutes from "./auth.routes.js";
 import profileRoutes from "./profile.routes.js";
 import planRoutes from "./plan.routes.js";
 import alumnoRoutes from "./alumno.routes.js";
-import sedeRoutes from "./sede.routes.js";
+//import sedeRoutes from "./sede.routes.js";
 
 export function routerApi(app) {
   const router = Router();
@@ -13,5 +13,5 @@ export function routerApi(app) {
   router.use("/profile", profileRoutes);
   router.use("/plans", planRoutes);
   router.use("/alumnos", alumnoRoutes);
-  router.use("/sedes", sedeRoutes);
+  //router.use("/sedes", sedeRoutes);
 }
