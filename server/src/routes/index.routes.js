45a264a01a52bc1d133d8134127242de6eea688e
jeanjeldres <jean.jeldres2101@alumnos.ubiobio.cl>
2026-06-03 22:54:20 -0400

    arreglado crud de profesores

--- server/src/routes/index.routes.js
+++ server/src/routes/index.routes.js
@@ -4,6 +4,9 @@ import profileRoutes from "./profile.routes.js";
 import planRoutes from "./plan.routes.js";
 import alumnoRoutes from "./alumno.routes.js";
 import sedeRoutes from "./sede.routes.js";
+import profesorRoutes from "./profesor.routes.js";
+import secretariaRoutes from "./secretaria.routes.js";
+import autoRoutes from "./auto.routes.js";
 
 export function routerApi(app) {
   const router = Router();
@@ -11,7 +14,10 @@ export function routerApi(app) {
 
   router.use("/auth", authRoutes);
   router.use("/profile", profileRoutes);
+  router.use("/autos", autoRoutes);
   router.use("/plans", planRoutes);
   router.use("/alumnos", alumnoRoutes);
   router.use("/sedes", sedeRoutes);
+  router.use("/profesores", profesorRoutes);
+  router.use("/secretarias", secretariaRoutes);
 }
