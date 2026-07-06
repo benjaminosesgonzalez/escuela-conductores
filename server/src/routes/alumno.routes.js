2f9b26945e9ceb250ba1c93665f55e865aadd845
jeanjeldres <jean.jeldres2101@alumnos.ubiobio.cl>
2026-07-05 21:52:43 -0400

    frontend de autos y solicitudes de

--- server/src/routes/alumno.routes.js
+++ server/src/routes/alumno.routes.js
@@ -14,6 +14,17 @@ const router = Router();
 // El alumno elige su preferencia POST /api/alumnos/preferencia
 router.post("/preferencia", authMiddleware, elegirPlanPreferencia);
 
+router.get("/estado-matricula", authMiddleware, async (req, res) => {
+  const idUser = req.user.id; // Suponiendo que el middleware de autenticación agrega el usuario al objeto req
+  try {
+    const estado = await obtenerEstadoMatriculaService(idUser);
+    res.json({ estado });
+  } catch (error) {
+    console.error("Error al obtener estado de matrícula:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+});
+
 // La secretaría oficializa la matrícula POST /api/alumnos/matricular
 router.post(
   "/matricular",
