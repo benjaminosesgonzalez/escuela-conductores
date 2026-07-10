7fbcbfc419176c31217b8dd315d7db25d5e4d097
Mauricio Giussepe Burgos Riquelme <mauricio.burgos2101@alumnos.ubiobio.cl>
2026-07-09 23:41:05 -0400

    Se añade vista en front para crud de secretarias en vista de admin

--- client/src/pages/admin/DashboardAdmin.jsx
+++ client/src/pages/admin/DashboardAdmin.jsx
@@ -11,6 +11,7 @@ import VehiculosView from "../secretaria/VehiculosView.jsx";
 import { Card, Button } from "../../components/shared/index.js";
 import { colors, spacing } from "../../theme/index.js";
 import { authService } from "../../services/authService.js";
+import SecretariasView from "./SecretariasView.jsx";
 
 const DashboardAdmin = () => {
   const [activeTab, setActiveTab] = useState("inicio");
@@ -28,6 +29,7 @@ const DashboardAdmin = () => {
 
   const currentUser = authService.getCurrentUser();
   const adminNombre = currentUser?.nombre || "Administrador";
+  const [secretarias, setSecretarias] = useState([]);
 
   useEffect(() => {
     const fetchDashboardData = async () => {
@@ -35,11 +37,12 @@ const DashboardAdmin = () => {
         const token = authService.getToken();
         const headers = { Authorization: `Bearer ${token}` };
 
-        const [alumnosRes, profesRes, autosRes, sedesRes] = await Promise.all([
+        const [alumnosRes, profesRes, autosRes, sedesRes, secretariasRes] = await Promise.all([
           fetch("http://localhost:5000/api/alumnos", { headers }),
           fetch("http://localhost:5000/api/profesores", { headers }),
           fetch("http://localhost:5000/api/autos", { headers }),
           fetch("http://localhost:5000/api/sedes", { headers }),
+          fetch("http://localhost:5000/api/secretarias", { headers })
         ]);
 
         const alumnosData = await alumnosRes.json();
@@ -74,6 +77,13 @@ const DashboardAdmin = () => {
           const sedesData = await sedesRes.json();
           if (sedesData.success) setSedesDisponibles(sedesData.data);
         }
+
+        if (secretariasRes.ok) {
+          const secretariasData = await secretariasRes.json();
+          if (secretariasData.success) {
+            setSecretarias(secretariasData.data);
+          }
+        }
       } catch (error) {
         console.error("Error cargando la data del administrador:", error);
       }
@@ -107,6 +117,13 @@ const DashboardAdmin = () => {
           profesores={profesores}
           setProfesores={setProfesores}
           sedesDisponibles={sedesDisponibles}
+        />
+      )}
+
+      {activeTab === "secretarias" && (
+        <SecretariasView 
+          secretarias={secretarias} 
+          setSecretarias={setSecretarias} 
         />
       )}
 
